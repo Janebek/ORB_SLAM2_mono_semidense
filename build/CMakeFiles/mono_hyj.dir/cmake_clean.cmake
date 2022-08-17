1d13f52d5938ed4d6ce203d21548d79a2cf4f341
Janebek <1181030951@qq.com>
2022-08-17 17:34:42 +0800

    add many things

---
+++ build/CMakeFiles/mono_hyj.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mono_hyj.dir/Examples/Monocular/mono_hyj.cpp.o"
+  "../Examples/Monocular/mono_hyj.pdb"
+  "../Examples/Monocular/mono_hyj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mono_hyj.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/mono_semidense.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mono_semidense.dir/Examples/Monocular/mono_semidense.cpp.o"
+  "../Examples/Monocular/mono_semidense.pdb"
+  "../Examples/Monocular/mono_semidense"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mono_semidense.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
